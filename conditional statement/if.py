81a89b12aef05b620e65fc89bf3a297ee686d971
SohiniChakraborty123 <129667761+SohiniChakraborty123@users.noreply.github.com>
2023-04-07 17:44:46 +0530

    Add files via upload

---
+++ conditional statement/if.py
@@ -0,0 +1,26 @@
+# conditional statements:
+# if[condition]:
+#     statements
+# elif[conditions]:
+#     statements
+# else:
+#     statements
+
+# example:
+num1=100
+num2= 200
+# 1> num1 is greater,2> num2 is greater,3>num1 and num2 are equal
+# if(num1>num2):
+#     print("Num1 is greater than num2")
+# elif(num2>num1):
+#     print("Num2 is greater than num1")
+# else:
+#     print("both are equal")
+
+# indention: every python block has indentation if we don't give that it will give error
+if 1:
+    print("statement in if block")
+else:
+    print("Statement in else block")
+# if we give any non zero value after if it will consider as true go to  if block but if we give 0 it will go to the else block
+
